7f22d0e093624f924f723b2a724025611f130686
Matthew Hsu <gears88@gmail.com>
2015-04-05 21:25:06 -0400

    refine college typeahead for case when lots of similar matches found

--- public/js/register.js
+++ public/js/register.js
@@ -1,12 +1,36 @@
 $('document').ready(function () {
-    //TODO: match from start of string
+    //TODO: improve sorting when lots of similar matches found
     var engine = new Bloodhound({
         name: 'colleges',
         prefetch: '/api/colleges',
         datumTokenizer: function (d) {
             return Bloodhound.tokenizers.whitespace(d.name);
         },
-        queryTokenizer: Bloodhound.tokenizers.whitespace
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        limit: 5,
+        sorter:function(a, b) {
+
+            //get input text
+            var InputString=   $('#college').val();
+            //move exact matches to top
+            if(InputString==a.name){ return -1;}
+            if(InputString==b.name){return 1;}
+
+            //close match without case matching
+            if(InputString.toLowerCase() ==a.name.toLowerCase()){ return -1;}
+            if(InputString.toLowerCase()==b.name.toLowerCase()){return 1;}
+
+            if( (InputString!=a.name) && (InputString!=b.name)){
+
+                if (a.name < b.name) {
+                    return -1;
+                }
+                else if (a.name > b.name) {
+                    return 1;
+                }
+                else return 0;
+            }
+        }
     });
 
     engine.initialize();
@@ -14,6 +38,7 @@ $('document').ready(function () {
     $('.typeahead').typeahead({
         hint: true,
         highlight: true,
+        autoselect: false,
         minLength: 1
     }, {
         displayKey: 'name', // if not set, will default to 'value',
