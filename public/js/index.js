c13ac2fe032892c5472972548d83d515c33c6742
Matthew Hsu <gears88@gmail.com>
2015-05-10 14:09:38 -0400

    random index cover photo dashboard styling changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,65 @@
+function pad(num, size) {
+    var s = "000" + num; //assume never need more than 3 digits
+    return s.substr(s.length - size);
+}
+
 (function ($) {
 
-	new WOW().init();
+    new WOW().init();
 
-	jQuery(window).load(function() { 
-		jQuery("#preloader").delay(100).fadeOut("slow");
-		jQuery("#load").delay(100).fadeOut("slow");
-	});
+    jQuery(window).load(function () {
+        jQuery("#preloader").delay(100).fadeOut("slow");
+        jQuery("#load").delay(100).fadeOut("slow");
+    });
 
+    //jQuery to collapse the navbar on scroll
+    $(window).scroll(function () {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
 
-	//jQuery to collapse the navbar on scroll
-	$(window).scroll(function() {
-		if ($(".navbar").offset().top > 50) {
-			$(".navbar-fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".navbar-fixed-top").removeClass("top-nav-collapse");
-		}
-	});
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function () {
+        $('.navbar-nav li a').bind('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1000, 'easeInOutExpo');
+            event.preventDefault();
+        });
+        $('.page-scroll a').bind('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1000, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
 
-	//jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$('.navbar-nav li a').bind('click', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 1000, 'easeInOutExpo');
-			event.preventDefault();
-		});
-		$('.page-scroll a').bind('click', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 1000, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
+    //properties correspond to indexed background images in /img/bgd
+    var images = [
+        {
+            navbar: "blue"
+        },
+        {
+            navbar: "orange"
+        },
+        {
+            navbar: "blue"
+        },
+        {
+            navbar: "blue"
+        }
+    ];
+
+    var numImages = images.length;
+    var index = Math.floor(Math.random() * numImages);
+    $('.intro').css({'background': 'url("/img/bgd/cornell_' + pad(index, 3) + '.jpg") no-repeat center center scroll',
+    'background-size': 'cover'});
+    $('.nav.navbar-nav').addClass(images[index].navbar)
+
 
 })(jQuery);
