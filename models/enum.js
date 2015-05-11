224891a18c9c38d7b7c500819b9f65e102c24820
Kevin Jiang <kzj1995@yahoo.com>
2015-05-11 15:40:51 -0400

    Academic Year

--- models/enum.js
+++ models/enum.js
@@ -6,7 +6,7 @@ Consistent list of enums used throughout the app
  */
 var en = {
     user: {
-        year: _generateYears(),
+        year: "Freshman/Sophomore/Junior/Senior/Graduate Student".split("/"),
         dietary: "None Vegetarian Gluten-free".split(" "),
         gender: "Female Male Other".split(" "),
         tshirt: "XS S M L XL".split(" "),
@@ -15,19 +15,5 @@ var en = {
         projecttype: "Not sure/Hardware/Software".split("/")
     }
 };
-
-/**
- * dynamically generate list of the next four years + any additional params
- * @returns {Array.<T>}
- * @private
- */
-function _generateYears() {
-    var other = "".split(" "); //specify other years, i.e. hs, grad, etc
-    if (other == "") {
-        other = [];
-    }
-    var year = new Date().getFullYear();
-    return other.concat(_.range(year, year + 4));
-}
 
 module.exports = en;
