bffd084b23069894e7e46760ddfef7d8b7d99341
Matthew Hsu <gears88@gmail.com>
2015-06-26 19:06:42 -0400

    [fix] dynamically check for registered email

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,22 @@
 "use strict";
 var express = require('express');
 var router = express.Router();
-var colleges  = require('../models/college.js');
+var Colleges = require('../../models/college.js');
+var User = require('../../models/user.js');
 
-
-router.get('/colleges', function(req, res, next) {
-    colleges.getAll(function(err, data) {
-        if (err) {
-            console.log(err);
-        }
-        res.send(data);
+router.get('/colleges', function (req, res, next) {
+    Colleges.getAll(function (err, data) {
+        if (err) console.log(err);
+        else res.send(data);
     });
 });
+
+router.get('/validEmail', function (req, res, next) {
+    User.findOne({email: req.query.email}, function (err, user) {
+        if (err) console.err(err);
+        else res.send(!user);
+    });
+});
+
 
 module.exports = router;
