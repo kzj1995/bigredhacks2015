d99540fd1cbfac8d4d75dfa725d54d343e23c105
Matthew Hsu <gears88@gmail.com>
2015-07-01 23:33:10 -0400

    [fix] refactored queryBuilder

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ var helper = require('../util/routes_helper');
 var User = require('../models/user.js');
 var enums = require('../models/enum.js');
 var config = require('../config.js');
-var searchable = require('../models/searchable.js');
+var queryBuilder = require('../util/search_query_builder.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -110,7 +110,7 @@ router.get('/search', function (req, res, next) {
     }
 
     //for a mapping of searchable fields, look at searchable.ejs
-    var query = _queryBuilder(req.query, searchable.user);
+    var query = queryBuilder(req.query, "user");
 
     /*
      * two types of search approaches:
@@ -156,80 +156,7 @@ router.get('/search', function (req, res, next) {
 ;
 
 //todo refactor this
-/**
- * Builds an aggregate or simple query
- * @param query Object Raw req.query input
- * @param searchSchema Searchable fields as defined in searchable.js
- * @return Object out
- * out.match: object to match
- * out.project: object to project if aggregation query
- */
-var _queryBuilder = function queryBuilder(query, searchSchema) {
-    var project = {};
-    var match = {};
-    _.each(_.omit(query, function (v, k) {
-            return (_.findWhere(searchSchema, {alias: k}) == undefined) || v == '';
-        }), function (v, k) {
-            //get path as array
-            var searchItem = _.findWhere(searchSchema, {alias: k});
 
-            //implicit path definition
-            var path;
-            if (typeof searchItem.path == "undefined") {
-                path = searchItem.alias;
-            }
-            else path = searchItem.path;
-            path = path.split(" "); //path must be array
-
-            //fuzzy search
-            var valToMatch;
-            if (searchItem.fuzzytext) {
-                //todo spaces at end of string break this
-                valToMatch = _toTextMatch(v.split(" "));
-            }
-            else valToMatch = v;
-
-            //generate projection
-            if (path.length > 1) {
-
-                // ex: {fullName: {$concat: ['$firstName', ' ', '$lastName']}}
-                //convert to projection notation and add space between elements
-                path = _.flatten(_.map(path, function (e) {
-                    return ["$" + e, " "];
-                }));
-                path.pop();//pop() last " " element resulting from map
-
-                project[k] = {'$concat': path};
-                match[k] = valToMatch;
-            }
-            else {
-                match[path] = valToMatch;
-            }
-        }
-    );
-
-    return {
-        project: project,
-        match: match
-    };
-};
-
-/**
- * convert an array of items to a text match regex
- * @param terms tokenized terms to alternate
- * @return regex with alternations between terms
- */
-
-var _toTextMatch = function _toTextMatch(terms) {
-    var regexString = "";
-
-    for (var i = 0; i < terms.length; i++) {
-        regexString += terms[i];
-        if (i < terms.length - 1) regexString += '|';
-    }
-
-    return new RegExp(regexString, 'ig');
-};
 
 router.get('/review', function (req, res, next) {
     res.render('admin/review', {
