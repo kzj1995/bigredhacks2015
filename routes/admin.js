cfee0806e87f771001a78633e130cd04a0677adb
Matthew Hsu <gears88@gmail.com>
2015-07-01 23:19:35 -0400

    [new] reimplemented search with working full text fuzzy matching. Searching is conducted based on a searchable schema (see ./models/searchable.js), and all future fields need to be addded to this schema.

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,12 @@ var express = require('express');
 var router = express.Router();
 var _ = require('underscore');
 var async = require('async');
-var passport = require('passport');
 var validator = require('../library/validations.js');
 var helper = require('../util/routes_helper');
 var User = require('../models/user.js');
 var enums = require('../models/enum.js');
 var config = require('../config.js');
+var searchable = require('../models/searchable.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -95,99 +95,141 @@ router.get('/team/:teamid', function (req, res, next) {
     })
 });
 
-router.get('/search', function (req, res, next) {
-    User.find({}).sort('name.last').exec(function (err, applicants) {
-        res.render('admin/search', {
-            title: 'Admin Dashboard - Search',
-            applicants: applicants
-        })
-    });
-});
 
 /* POST Search based on inputted fields */
-router.post('/search', function (req, res, next) {
-    //Boolean variables indicating whether a field was searched for
-    var collegesearched, useridsearched, emailsearched, namesearched;
-    //If name was searched, get the first and last name
-    var firstname, lastname;
-    if (req.body.college != "") {
-        collegesearched = true
-    }
-    if (typeof req.body.userid != "undefined" && req.body.userid != "") {
-        useridsearched = true
-    }
-    if (typeof req.body.email != "undefined" && req.body.email != "") {
-        emailsearched = true
-    }
-    if (typeof req.body.name != "undefined" && req.body.name != "") {
-        namesearched = true;
-        var name = req.body.name.split(" ");
-        firstname = name[0];
-        lastname = name[1];
-    }
-    if (collegesearched && useridsearched) {
-        User.find({'school.name': req.body.college, pubid: req.body.userid}).sort
-        ('name.last').exec(function (err, applicants) {
+//todo refactor query execution logic
+router.get('/search', function (req, res, next) {
+
+    if (!req.query) {
+        User.find({}).sort('name.last').limit(100).exec(function (err, applicants) {
             res.render('admin/search', {
                 title: 'Admin Dashboard - Search',
                 applicants: applicants
             })
         });
     }
-    else if (collegesearched && emailsearched) {
-        User.find({'school.name': req.body.college, email: req.body.email}).sort
-        ('name.last').exec(function (err, applicants) {
-            res.render('admin/search', {
-                title: 'Admin Dashboard - Search',
-                applicants: applicants
-            })
-        });
-    }
-    else if (collegesearched && namesearched) {
-        User.find({'school.name': req.body.college, 'name.first': firstname, 'name.last': lastname}).sort
-        ('name.last').exec(function (err, applicants) {
-            res.render('admin/search', {
-                title: 'Admin Dashboard - Search',
-                applicants: applicants
-            })
-        });
-    }
-    else if (collegesearched) {
-        User.find({'school.name': req.body.college}).sort('name.last').exec(function (err, applicants) {
-            res.render('admin/search', {
-                title: 'Admin Dashboard - Search',
-                applicants: applicants
-            })
-        });
-    }
-    else if (useridsearched) {
-        User.find({pubid: req.body.userid}).sort('name.last').exec(function (err, applicants) {
-            res.render('admin/search', {
-                title: 'Admin Dashboard - Search',
-                applicants: applicants
-            })
-        });
-    }
-    else if (emailsearched) {
-        User.find({email: req.body.email}).sort('name.last').exec(function (err, applicants) {
-            res.render('admin/search', {
-                title: 'Admin Dashboard - Search',
-                applicants: applicants
-            })
-        });
-    }
-    else if (namesearched) {
-        User.find({'name.first': firstname, 'name.last': lastname}).sort('name.last').exec(function (err, applicants) {
-            res.render('admin/search', {
-                title: 'Admin Dashboard - Search',
-                applicants: applicants
-            })
-        });
+
+    //for a mapping of searchable fields, look at searchable.ejs
+    var query = _queryBuilder(req.query, searchable.user);
+
+    /*
+     * two types of search approaches:
+     * 1. simple query (over single fields)
+     * 2. aggregate query (over fields that need implicit joins)
+     */
+    if (_.size(query.project) > 0) {
+        query.project.document = '$$ROOT'; //return the actual document
+        query.project.lastname = '$name.last'; //be able to sort by last name
+        console.log(query);
+        User.aggregate()
+            .project(query.project)
+            .match(query.match)
+            .sort('lastname')
+            .exec(function (err, applicants) {
+                if (err) endOfCall(err);
+                else {
+                    endOfCall(err, _.map(applicants, function (x) {
+                        return x.document
+                    }));
+                }
+            });
     }
     else {
-        res.redirect('/admin/search');
+        //run a simple query (because it's faster)
+        User.find(query.match)
+            .sort('name.last')
+            .exec(endOfCall);
     }
-});
+
+    function endOfCall(err, applicants) {
+        console.log(applicants);
+        if (err) console.error(err);
+        else {
+            res.render('admin/search', {
+                title: 'Admin Dashboard - Search Results',
+                applicants: applicants
+            })
+        }
+    }
+
+})
+;
+
+//todo refactor this
+/**
+ * Builds an aggregate or simple query
+ * @param query Object Raw req.query input
+ * @param searchSchema Searchable fields as defined in searchable.js
+ * @return Object out
+ * out.match: object to match
+ * out.project: object to project if aggregation query
+ */
+var _queryBuilder = function queryBuilder(query, searchSchema) {
+    var project = {};
+    var match = {};
+    _.each(_.omit(query, function (v, k) {
+            return (_.findWhere(searchSchema, {alias: k}) == undefined) || v == '';
+        }), function (v, k) {
+            //get path as array
+            var searchItem = _.findWhere(searchSchema, {alias: k});
+
+            //implicit path definition
+            var path;
+            if (typeof searchItem.path == "undefined") {
+                path = searchItem.alias;
+            }
+            else path = searchItem.path;
+            path = path.split(" "); //path must be array
+
+            //fuzzy search
+            var valToMatch;
+            if (searchItem.fuzzytext) {
+                //todo spaces at end of string break this
+                valToMatch = _toTextMatch(v.split(" "));
+            }
+            else valToMatch = v;
+
+            //generate projection
+            if (path.length > 1) {
+
+                // ex: {fullName: {$concat: ['$firstName', ' ', '$lastName']}}
+                //convert to projection notation and add space between elements
+                path = _.flatten(_.map(path, function (e) {
+                    return ["$" + e, " "];
+                }));
+                path.pop();//pop() last " " element resulting from map
+
+                project[k] = {'$concat': path};
+                match[k] = valToMatch;
+            }
+            else {
+                match[path] = valToMatch;
+            }
+        }
+    );
+
+    return {
+        project: project,
+        match: match
+    };
+};
+
+/**
+ * convert an array of items to a text match regex
+ * @param terms tokenized terms to alternate
+ * @return regex with alternations between terms
+ */
+
+var _toTextMatch = function _toTextMatch(terms) {
+    var regexString = "";
+
+    for (var i = 0; i < terms.length; i++) {
+        regexString += terms[i];
+        if (i < terms.length - 1) regexString += '|';
+    }
+
+    return new RegExp(regexString, 'ig');
+};
 
 router.get('/review', function (req, res, next) {
     res.render('admin/review', {
