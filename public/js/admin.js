37f5eb0d508cccf72c90971d66d124169dcd6d9b
Matthew Hsu <gears88@gmail.com>
2015-07-06 23:24:47 -0400

    [new] added ability to change render preferences [fix] formatting

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,6 +1,44 @@
 $('document').ready(function () {
 
 
+    //generic ajax to update status
+    var updateStatus = function updateStatus(pubid, newStatus, callback) {
+        $.ajax({
+            type: "PATCH",
+            url: "/api/admin/user/" + pubid + "/setStatus",
+            data: {
+                status: newStatus
+            },
+            success: function (data) {
+                callback(data);
+            },
+            error: function (e) {
+                //todo more descriptive errors
+                console.log("Update failed!");
+            }
+        });
+    };
+
+    var updateUrlParam = function updateUrlParam(url, param, paramVal){
+        var newAdditionalURL = "";
+        var tempArray = url.split("?");
+        var baseURL = tempArray[0];
+        var additionalURL = tempArray[1];
+        var temp = "";
+        if (additionalURL) {
+            tempArray = additionalURL.split("&");
+            for (var i=0; i<tempArray.length; i++){
+                if(tempArray[i].split('=')[0] != param){
+                    newAdditionalURL += temp + tempArray[i];
+                    temp = "&";
+                }
+            }
+        }
+        var rows_txt = temp + "" + param + "=" + paramVal;
+        return baseURL + "?" + newAdditionalURL + rows_txt;
+    };
+
+    //handle decision buttons
     $(".decisionbuttons button").click(function () {
         var _this = this;
         var buttongroup = $(this).parent();
@@ -10,25 +48,33 @@ $('document').ready(function () {
 
         $(buttons).prop("disabled", true).removeClass("active");
 
-        $.ajax({
-            type: "PATCH",
-            url: "/api/admin/user/" + pubid + "/setStatus",
-            data: {
-                status: newStatus
-            },
-            success: function (data) {
-                $(_this).parent().siblings(".status-text").text(newStatus);
-                $(buttons).prop("disabled", false);
-                $(_this).addClass("active");
-            },
-            error: function (e) {
-                //todo more descriptive errors
-                console.log("Update failed!");
-            }
+        updateStatus(pubid, newStatus, function (data) {
+            $(_this).parent().siblings(".status-text").text(newStatus);
+            $(buttons).prop("disabled", false);
+            $(_this).addClass("active");
         });
+
 
     });
 
+    //handle decision radio buttons
+    $('input[type=radio][name=status]').on('change', function () {
+        var _this = this;
+        var newStatus = $(_this).val();
+        var radios = $(_this).parents(".decision-radio").find("input[type=radio]");
+        var pubid = $(_this).parents(".applicant").data("pubid");
+
+        $(radios).prop("disabled", true);
+        updateStatus(pubid, newStatus, function (data) {
+            $(radios).prop("disabled", false);
+        })
+    });
+
+    //switch render location
+    $('#render').on('change', function() {
+        var redirect = updateUrlParam(window.location.href, "render", $(this).val());
+        window.location.assign(redirect);
+    });
     var searchCategories = {
         pubid: {
             name: "pubid",
